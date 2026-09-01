bdec630ebceab4d71ef40820709f8aca6210cbb0
aya <ayaaa70@gmail.com>
2026-09-01 13:23:50 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,45 @@ const observer = new IntersectionObserver((entries) => {
 collectionItems.forEach((item) => {
     observer.observe(item);
 });
+
+const scrollDot = document.querySelector(".scroll-dot");
+
+window.addEventListener("scroll", () => {
+
+    const scrollTop = window.scrollY;
+    const pageHeight = document.documentElement.scrollHeight;
+    const windowHeight = window.innerHeight;
+
+    const scrollableHeight = pageHeight - windowHeight;
+
+    const scrollProgress = scrollTop / scrollableHeight;
+
+    const lineHeight = 170;
+
+    scrollDot.style.transform =
+        `translateX(-50%) translateY(${scrollProgress * lineHeight}px)`;
+
+});
+
+const sections = document.querySelectorAll("main > section");
+const scrollNumber = document.querySelector(".scroll-number-top");
+
+window.addEventListener("scroll", () => {
+
+    const center = window.innerHeight / 2;
+    let currentSection = 1;
+
+    sections.forEach((section, index) => {
+
+        const rect = section.getBoundingClientRect();
+
+        if (rect.top <= center) {
+            currentSection = index + 1;
+        }
+
+    });
+
+    scrollNumber.textContent =
+        String(currentSection).padStart(2, "0");
+
+});
